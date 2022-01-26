8e5c11279dcae915c1c6ae0139e36f04385358e5
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-26 16:01:46 +0700

    Initial Commit

---
+++ TextAnimation/TextAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextAnimationApp.swift
+//  TextAnimation
+//
+//  Created by Quang Bao on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TextAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

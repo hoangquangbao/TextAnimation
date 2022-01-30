11524cdb2500631a0f8e83ab90e4921e12887e33
Quang Bao <hoangquangbao.developer@gmail.com>
2022-01-30 16:16:54 +0700

    Set Text and Size

--- TextAnimation/View/Home.swift
+++ TextAnimation/View/Home.swift
@@ -27,7 +27,8 @@ struct Home: View {
                 }
                 .frame(height: 220)
                 
-                Text("With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life.")
+                Marquee(text: "With the blazing-fast M1 Pro or M1 Max chip — the first Apple silicon designed for pros — you get groundbreaking performance and amazing battery life.", font: .systemFont(ofSize: 16, weight: .light))
+                
             }
             .padding(.horizontal)
             .vTop()
@@ -46,5 +47,41 @@ extension View{
     
     func vTop() -> some View {
         frame(maxHeight: .infinity, alignment: .top)
+    }
+}
+
+// MARK: Marquee Text View
+struct Marquee: View {
+    
+    var text: String
+    // Customimazation Options
+    var font: UIFont
+    
+    // Storing Text Size
+    @State var storedSize: CGSize = .zero
+    
+    var body: some View{
+        
+        // Since it scroll horizontal using ScrollView
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+            Text(text)
+                .font(Font(font))
+        }
+        // Disabling Manual Scrolling
+        .disabled(true)
+        .onAppear {
+            
+            storedSize = textSize()
+        }
+    }
+    
+    // MARK: Fetching Text Size for Offset Animation
+    func textSize() -> CGSize {
+        
+        let attributes = [NSAttributedString.Key.font: font]
+        let size = (text as NSString).size(withAttributes: attributes)
+        
+        return size
     }
 }
